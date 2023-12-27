9d7f9d780c1b42450431cfeb81abd43d6102f0e9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-28 00:46:32 +0100

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,6 +6,6 @@ package { 'Python3-pip':
 
 exec {'install_flask':
     command => '/usr/bin/pip3 install flask==2.1.0',
-    unless  => '/usr/bin/pip show flask | prep killmenow'
+    unless  => '/usr/bin/pip show flask | pgrep 2.1.0'
     require => Package['Python3-pip']
 }
