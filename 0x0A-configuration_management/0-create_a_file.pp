a97c0e6da55d41a25514a07ab1b0dd045be08551
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-27 23:30:58 +0100

    puppet to create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -5,7 +5,7 @@ $str = "I love Puppet
 
 file { '/tmp/school':
     ensure  => present,
-    mode    => '0744'
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => $str
