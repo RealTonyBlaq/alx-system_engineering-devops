f3661347b6a3395b6d8fc855fbc2e41182cb8949
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-12-28 23:58:34 +0100

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,7 @@
 # Puppet installs flask from pip3
 
-package { 'werkzeug':
-  ensure => '2.1.1',
-}
-
-package { 'python3':
-  ensure => '3.8.10',
-  require => Package['werkzeug'],
-}
-
-package { 'flask':
-  ensure => '2.1.0',
-  unless => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3'],
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
 
