20b041d8ef1a395cb9d610234a444e7adf1876b5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-24 02:41:45 +0100

    nm

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -15,11 +15,13 @@ package {'nginx':
 file {'/var/www/html/index.html':
     ensure  => present,
     content => '$str',
+    require => Package['nginx'],
 }
 
 exec {'write_to_file':
     command  => 'sed -i "24i\      rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
     provider => 'shell',
+    require  => Package['nginx'],
 }
 
 service {'nginx':
