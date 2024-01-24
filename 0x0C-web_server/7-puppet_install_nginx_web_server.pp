2baf061e4dd3043104d4b6d4615823a9bb4325ca
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-24 02:07:26 +0100

    my pup

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -15,7 +15,7 @@ package {'nginx':
 }
 
 exec {'write_to_file':
-    command => 'sudo sed -i "24i\      rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+    command => ['sudo sed -i "24i\      rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default'],
     require => Package['nginx'],
 }
 
