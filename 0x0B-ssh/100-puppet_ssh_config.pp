843e2ac2fbc9758bda1f7e8336a1bea71cb6c5f0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-25 14:10:39 +0100

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,7 @@
 # Puppet sets up Client ssh configuration to read public key
 #+ from a file without typing a password
 
-$config="
-#! ~/.ssh/config
+$config="#! ~/.ssh/config
 
 Host *
     IdentityFile ~/.ssh/school
@@ -10,7 +9,7 @@ Host *
     PasswordAuthentication no
 "
 
-file {'~/.ssh/config':
+file {'/home/tony/con':
     ensure  => present,
     content => $config,
 }
