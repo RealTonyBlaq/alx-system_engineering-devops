b42c03810f0b2bdce525a3239273f28557311f25
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-01-25 14:41:14 +0100

    fix puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,18 @@
 # Puppet sets up Client ssh configuration to read public key
 #+ from a file without typing a password
 
-$config="#! ~/.ssh/config
-
-Host *
-    IdentityFile ~/.ssh/school
-    PreferredAuthentications publickey
-    PasswordAuthentication no
-"
-
-file {'/home/tony/con':
-    ensure  => present,
-    content => $config,
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => template('your_module/ssh_config.erb'),
 }
+
+# ssh_config.erb
+<%# Use Augeas to edit the SSH client configuration file %>
+<% require 'augeas' %>
+<% augeas = Augeas::open(nil, nil, Augeas::NO_MODL_AUTOLOAD) %>
+<% augeas.transform(:lens => 'Ssh.lns', :incl => '/etc/ssh/ssh_config') %>
+<% augeas.load %>
+<% augeas.set('/etc/ssh/ssh_config/Host[1]/IdentityFile', '~/.ssh/school') %>
+<% augeas.set('/files/home/your_username/.ssh/config/Host[1]/PasswordAuthentication', 'no') %>
+<% augeas.save %>
+<% augeas.close %>
