5db7755790122f43a9db6ca19eb68053cef92223
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-05 21:51:34 +0100

    add header setup manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,4 +2,15 @@
 
 exec {'read_hostname':
     command => 'hostname',
+    provider 
 }
+
+exec { 'get_variable_value':
+  command     => '/path/to/your/command',
+  logoutput   => true,
+  refreshonly => true,
+  unless      => 'test -f /path/to/your/flag/file',
+  notify      => Notify['variable_notification'],
+}
+
+$my_variable = file('/path/to/your/output/file').content
