aaadb093340105bc1b681d87b70014d3b934c74a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-06 19:00:58 +0100

    buidl

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 # Puppet installs Nginx and adds a new header 'X-Served-By'
 
 $host_name = exec {'read_hostname':
-    command  => 'hostname',
-    provider => 'shell',
-    logoutput   => true,
+    command   => 'hostname',
+    provider  => 'shell',
+    logoutput => true,
 }.content
 
 package {'nginx':
