36910830a0d70efbd9c11d4303600a7f6b59eb31
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-06 19:44:17 +0100

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,16 +10,6 @@ package { 'nginx':
         require => Exec['update system']
 }
 
-file {'/var/www/html/index.html':
-        ensure  => present,
-        content => 'Hello World!'
-}
-
-exec {'redirect_me':
-        command  => 'sed -i "24i\         rewrite ^ https://th3-grOOt.tk/ permanent;" /etc/nginx/sites-available/default',
-        provider => 'shell'
-}
-
 exec {'HTTP header':
         command  => 'sed -i "25i\          add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
         provider => 'shell'
