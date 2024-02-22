45e1cdae22639b875a91cc4f054a9c6cfa77bad4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-23 00:54:32 +0100

    csv'ed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -23,5 +23,5 @@ if __name__ == "__main__":
             writer = csv.writer(f)
             for dic in todos:
                 if (dic['userId']) == id:
-                    dump = [id, f"\"{name}\"", f"\"{dic['completed']}\"", f"\"{dic['title']}\""]
+                    dump = [f""{id}"", f""{name}"", f""{dic['completed']}\"", f"\"{dic['title']}\""]
                     writer.writerow(dump)
