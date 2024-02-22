499f198d9afd77509a65bb1268fb876d84b7c19c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-23 00:02:42 +0100

    get API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -27,4 +27,4 @@ if __name__ == "__main__":
                         completed += 1
                         title.append(key['title'])
     print(f"Employee {name} is done with tasks({completed}/{tasks}):")
-    print("")
+    print("     {}".format()) for 
