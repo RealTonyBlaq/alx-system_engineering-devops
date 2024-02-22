0afd9bcdd7eb97949f4b0ff0aa430d75178d7b94
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 22:11:36 +0100

    gather info API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,5 +15,6 @@ if __name__ == "__main__":
         id = int(argv[1])
         url = "https://jsonplaceholder.typicode.com"
         employee = requests.get("{}/{}/{}".format(url, "users", id)).json()
-        todos = requests.get("{}/{}".format("todos", None))
+        print(employee)
+        todos = requests.get("{}/{}".format(url, "todos"))
         print(todos.json())
