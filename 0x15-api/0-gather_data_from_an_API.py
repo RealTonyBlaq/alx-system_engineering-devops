a87d18f1b9097211b469397eb56327ecd7f1fae9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 21:33:58 +0100

    gather info API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 """ Script uses a REST API, for a given employee ID,
-and returns information about his/her T list progress."""
+and returns information about his/her TODO list progress
+"""
+
+import requests
+i
+
+
+
