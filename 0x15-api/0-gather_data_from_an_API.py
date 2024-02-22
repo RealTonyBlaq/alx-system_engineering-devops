46b4b87e147d2309fe1ca67349ab9cd5ec45a469
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 22:00:08 +0100

    gather info API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     else:
         id = int(argv[1])
         url = "https://jsonplaceholder.typicode.com"
-        r = requests.get(url)
-        print(r.text)
+        json = requests.get(url)
+        print(r.json())
