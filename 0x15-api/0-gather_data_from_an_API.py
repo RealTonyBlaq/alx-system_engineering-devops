1764f14787927b20cc6863a5f224a7f206c835a8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 23:53:41 +0100

    get API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,6 +14,13 @@ if __name__ == "__main__":
     else:
         id = int(argv[1])
         url = "https://jsonplaceholder.typicode.com"
-        employee = requests.get("{}/{}/{}".format(url, "users", id)).json()
-        todos = requests.get("{}/{}".format(url, "todos"))
-        print(todos.json())
+        employee = requests.get("{}/{}/{}".format(url, "users", id)).json()['name']
+        todos = requests.get("{}/{}".format(url, "todos")).json()
+        tasks = 0
+        tasks_completed = 0
+        for dic in todos:
+            for key in dic.keys():
+                if key['userId'] == id:
+                    tasks += 1
+                    if key['completed'] == True:
+                        tasks_completed += 1
