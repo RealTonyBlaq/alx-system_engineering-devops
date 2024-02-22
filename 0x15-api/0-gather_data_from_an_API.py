a49d4113c18d5f0af664be6f0e42a1356e54c2e4
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-22 21:54:07 +0100

    gather info API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,7 +9,8 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 2:
-        print("Usage: ./0-gather_data_r")
+        print("Usage: ./0-gather_data_from_an_API.py <employee_ID>")
+        return None
     id = int(argv[1])
     REST_API = "https://jsonplaceholder.typicode.com/todos/{}".format(id)
     r = requests.get(REST_API)
