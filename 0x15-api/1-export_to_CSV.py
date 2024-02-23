85bd30a8da7f25a036206e0ae4a10eed17036869
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-23 01:09:06 +0100

    csv'ed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -21,6 +21,5 @@ if __name__ == "__main__":
         with open(file, '+a') as f:
             for dic in todos:
                 if (dic['userId']) == id:
-                    dump = f'"{id}","{name}","{dic["completed"]}","{dic[
-                        "title"]}"\n'
+                    dump = f'"{id}","{name}","{dic["completed"]}","{dic["title"]}".
                     f.write(dump)
