2ebad8001796673a49f6f1b5d734c674aedd7c2b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-24 08:06:43 +0100

    all employess to JSON

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -14,12 +14,13 @@ if __name__ == "__main__":
     todos = requests.get("{}/{}".format(url, "todos")).json()
     export = {}
     export_value = []
+    for em
     for dic in todos:
         if dic.get('userId') == id:
-                new = {"task": dic.get('title'),
-                       "completed": dic.get('completed'),
-                       "username": username}
-                export_value.append(new)
-        export['{}'.format(id)] = export_value
+            new = {"task": dic.get('title'),
+                   "completed": dic.get('completed'),
+                   "username": username}
+            export_value.append(new)
+    export['{}'.format(id)] = export_value
     with open("todo_all_employees.json", 'w') as f:
         f.write(json.dumps(export))
