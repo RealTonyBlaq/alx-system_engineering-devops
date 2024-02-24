28f1763e9af38662af389ee79d107c38a52c3f8d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-24 07:38:00 +0100

    to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         todos = requests.get("{}/{}".format(url, "todos")).json()
         file = "{}.json".format(id)
         with open(file, 'w') as f:
-            export = {}
+            exporte = {}
             export_value = []
             for dic in todos:
                 if dic.get('userId') == id:
@@ -29,5 +29,5 @@ if __name__ == "__main__":
                            "completed": dic.get('completed'),
                            "username": username}
                     export_value.append(new)
-            export['{}'.format(id)] = export_value
-            f.write(json.dumps(export))
+            exporte['{}'.format(id)] = export_value
+            f.write(json.dumps(exporte))
