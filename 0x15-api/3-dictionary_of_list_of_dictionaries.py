094f110ca799507f45d698c8484b8a3703097632
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-24 08:00:48 +0100

    all employess to JSON

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,19 +11,19 @@ from sys import argv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
-    all = requests.get("{}/{}/{}".format(
-        url, "users", id)).json()
-        print(all)
-        """todos = requests.get("{}/{}".format(url, "todos")).json()
-        file = "{}.json".format(id)
-        with open(file, 'w') as f:
-            export = {}
-            export_value = []
-            for dic in todos:
-                if dic.get('userId') == id:
-                    new = {"task": dic.get('title'),
-                           "completed": dic.get('completed'),
-                           "username": username}
+    all = requests.get("{}/{}".format(
+        url, "users")).json()
+    print(all)
+    """todos = requests.get("{}/{}".format(url, "todos")).json()
+    file = "{}.json".format(id)
+    with open(file, 'w') as f:
+        export = {}
+        export_value = []
+        for dic in todos:
+            if dic.get('userId') == id:
+                new = {"task": dic.get('title'),
+                       "completed": dic.get('completed'),
+                       "username": username}
                     export_value.append(new)
             export['{}'.format(id)] = export_value
             f.write(json.dumps(export))"""
