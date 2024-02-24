06fbe6916af210d26dfd4c3942901a58ad6b9075
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-24 07:36:34 +0100

    to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,20 +10,24 @@ import json
 
 
 if __name__ == "__main__":
-    id = int(argv[1])
-    url = "https://jsonplaceholder.typicode.com"
-    username = requests.get("{}/{}/{}".format(
-        url, "users", id)).json().get('username')
-    todos = requests.get("{}/{}".format(url, "todos")).json()
-    file = "{}.json".format(id)
-    export = {}
-    with open(file, 'w') as f:
-        export_value = []
-        for dic in todos:
-            if dic.get('userId') == id:
-                new = {"task": dic.get('title'),
-                       "completed": dic.get('completed'),
-                       "username": username}
-                export_value.append(new)
-        export['{}'.format(id)] = export_value
-        f.write(json.dumps(export))
+    if len(argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_ID>")
+        exit(1)
+    else:
+        id = int(argv[1])
+        url = "https://jsonplaceholder.typicode.com"
+        username = requests.get("{}/{}/{}".format(
+            url, "users", id)).json().get('username')
+        todos = requests.get("{}/{}".format(url, "todos")).json()
+        file = "{}.json".format(id)
+        export = {}
+        with open(file, 'w') as f:
+            export_value = []
+            for dic in todos:
+                if dic.get('userId') == id:
+                    new = {"task": dic.get('title'),
+                           "completed": dic.get('completed'),
+                           "username": username}
+                    export_value.append(new)
+            export['{}'.format(id)] = export_value
+            f.write(json.dumps(export))
