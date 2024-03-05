19035300b5370a4094d16b47465b43446427cf9f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-05 21:22:01 +0100

    query subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,3 +3,9 @@
 Script queries the Reddit API and returns the number of
 subscribers (not active users, total subscribers) for a given subreddit
 """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Returns number of total subscribers """
