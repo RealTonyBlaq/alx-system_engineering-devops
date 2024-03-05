6c8c9f49e8b092a07a98dd8a3cb306f640a3598a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-05 21:24:12 +0100

    query subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,5 +20,4 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
-    else:
-        return 0
+    return 0
