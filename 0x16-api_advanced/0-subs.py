c902e07028b1482896782f3f8eeb8d459a82513d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-05 21:23:29 +0100

    query subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,4 +9,16 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Returns number of total subscribers """
-    
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        'AppleWebKit/537.36'
+        '(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'Edg/91.0.864.59'
+    }
+    response = requests.get(URL, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
