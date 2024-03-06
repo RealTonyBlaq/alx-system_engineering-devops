1452259add7ba8d7e682069b4d8758fbc4d72239
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-06 23:56:47 +0100

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -14,7 +14,18 @@ service { 'apache2':
     require => Package['apache2'],
 }
 
+ # Set the correct permissions for directories
+file { '/etc/apache2':
+    ensure  => directory,
+    recurse => true,
+    mode    => '0755',
+}
 
+# Set the correct permissions for files
+  file { '/path/to/your/web/root':
+    ensure => file,
+    mode   => '0644',
+  }
 
 exec { 'fix_error':
     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
