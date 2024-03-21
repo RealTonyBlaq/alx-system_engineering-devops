491a7c95a8820ade3f5b4bc0d5638f9090f2bb4c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-21 20:05:16 +0100

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,7 @@
 # Puppet increases limits for user holberton
 
+
+
 exec { 'increase_soft_limits':
     command => 'sed -i "$a\holberton soft nofile 65535" /etc/security/limits.conf',
     path    => '/usr/bin/',
