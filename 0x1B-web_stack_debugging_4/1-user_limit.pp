7141be0cc719cd1a5779c19a862020477d018494
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-21 19:43:13 +0100

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # Puppet creates a user "holberton" and increases limits
 
 exec { 'increase_limit':
-    command => 'sed -i "/holberton        hard/s/5/100000" /etc/security/limits.conf',
+    command => 'sed -i "/holberton        hard/s/5/10000" /etc/security/limits.conf',
     path    => '/usr/bin',
 }
 
 exec { 'increase_soft':
-    command => 'sed -i "/holberton        soft/s/4/'
+    command => 'sed -i "/holberton        soft/s/4/20000" /etc/security/limits.conf',
 }
