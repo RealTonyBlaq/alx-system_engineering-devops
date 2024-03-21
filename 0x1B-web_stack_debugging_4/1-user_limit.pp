b71bd0663e03a3677a826f7af07a0914ad570468
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-21 20:06:44 +0100

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -8,10 +8,11 @@ user { 'holberton':
 exec { 'increase_soft_limits':
     command => 'sed -i "$a\holberton soft nofile 65535" /etc/security/limits.conf',
     path    => '/usr/bin/',
-    req
+    require => User['holberton'],
 }
 
 exec { 'increase_hard_limits':
     command => 'sed -i "$a\holberton hard nofile 65535" /etc/security/limits.conf',
     path    => '/usr/bin/',
+    require => User['holberton'],
 }
